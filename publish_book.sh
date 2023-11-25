ad9e62a2b0e7d59063f2f1361cdd43fcaa94bde6
Aaron Newman <63217782+aaronjnewman@users.noreply.github.com>
2023-11-25 19:49:24 -0400

    set env var to disable useless warnings

--- publish_book.sh
+++ publish_book.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
+
+export PYDEVD_DISABLE_FILE_VALIDATION=1
+
 # jupyter-book clean .
+
 jupyter-book build .
+
 ghp-import -n -p -f ./_build/html --cname='neuraldatascience.io'
+
 git add *
 git commit -m'Built book'
 git push origin master
